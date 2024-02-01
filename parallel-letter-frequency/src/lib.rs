027d25cc85cee7e3580b969506f6333fc718ea5f
anhnt <anhntbk08@gmail.com>
2024-02-01 16:24:33 +0700

    [parallel-letter-frequency]

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -1,11 +1,35 @@
 use std::collections::HashMap;
+use std::thread;
+use std::sync::{Arc, Mutex};
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    todo!(
-        "Count the frequency of letters in the given input '{input:?}'. Ensure that you are using {} to process the input.",
-        match worker_count {
-            1 => "1 worker".to_string(),
-            _ => format!("{worker_count} workers"),
-        }
-    );
+    let input : Vec<String> = input.iter().map(|str| str.to_string()).collect();
+    let result = Arc::new(Mutex::new(HashMap::new()));
+    let mut handles = vec![];
+
+    for iw in 0..worker_count  {
+        let input_clone = input.clone();
+        let result_clone = result.clone();
+        handles.push(thread::spawn(move || {
+            let start = iw * input_clone.len() / worker_count;
+            let end = if iw == worker_count - 1 {
+                input_clone.len()
+            } else {
+                (iw + 1) * input_clone.len() / worker_count
+            };
+
+            for line in &input_clone[start..end] {
+                let mut res = result_clone.lock().unwrap();
+                for c in line.chars().filter(|c| c.is_alphabetic()) {
+                    *res.entry(c.to_lowercase().next().unwrap()).or_insert(0) += 1;
+                }
+            }
+        }))
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    Arc::try_unwrap(result).expect("").into_inner().unwrap()
 }
